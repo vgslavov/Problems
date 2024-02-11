082c7bc08b7573e01692c1fbc69fd6bfa23ae8fe
Vasil Slavov <vgslavov@umkc.edu>
2024-02-10 21:36:42 -0500

    Add cheatsheets

---
+++ leetcode/longest_common_prefix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lowercase English letters.
+def longest_common_prefix(strs):
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
