fb6361c2ce1ef7fdcd1edf12ebc7e1064aa7aa75
Vasil Slavov <vgslavov@umkc.edu>
2024-02-17 21:02:41 -0500

    Add ransom note

---
+++ leetcode/can_construct.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import sys
+import unittest
+
+# 1 <= ransom_note.length, magazine.length <= 10^5
+# ransom_note and magazine consist of lowercase English letters.
+
+# O(n): slow
+def can_construct1(ransom_note, magazine):
+    letters = defaultdict(int)
+
+    for c in magazine:
+        letters[c] += 1
+
+    for c in ransom_note:
+        if c not in letters or not letters[c]:
+            return False
+        else:
+            letters[c] -= 1
+
+    return True
+
+# TODO: faster
+def can_construct2(ransom_note, magazine):
+    pass
+
+class TestCanConstruct(unittest.TestCase):
+
+    def test_empty(self):
+        note = ''
+        mag = ''
+        self.assertTrue(can_construct1(note, mag))
+
+    def test_false1(self):
+        note = "a"
+        mag = "b"
+        self.assertFalse(can_construct1(note, mag))
+
+    def test_false2(self):
+        note = "aa"
+        mag = "ab"
+        self.assertFalse(can_construct1(note, mag))
+
+    def test_true(self):
+        note = "aa"
+        mag = "aab"
+        self.assertTrue(can_construct1(note, mag))
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
