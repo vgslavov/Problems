f62399373671aa2ee4adfd40e772749d8681b95b
Vasil Slavov <vgslavov@umkc.edu>
2024-02-25 21:29:09 -0500

    Add contains dupes 2

---
+++ leetcode/ishappy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# A happy number is a number defined by the following process:
+# * Starting with any positive integer, replace the number by the sum of the
+#   squares of its digits.
+# * Repeat the process until the number equals 1 (where it will stay), or it
+#   loops endlessly in a cycle which does not include 1.
+# * Those numbers for which this process ends in 1 are happy.
+
+# 1 <= n <= 2^31 - 1
+def ishappy(n):
+    pass
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
