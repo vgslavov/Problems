531a90ba47df600a57fe1f6558bf2aef7b2c6d09
Vasil Slavov <vgslavov@umkc.edu>
2024-02-25 21:29:09 -0500

    Add contains dupes 2

---
+++ leetcode/contains_duplicate2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import sys
+import unittest
+
+# return true if there are two distinct indices i and j in the array such that
+# nums[i] == nums[j] and abs(i - j) <= k
+#
+# 1 <= nums.length <= 10^5
+# -10^9 <= nums[i] <= 10^9
+# 0 <= k <= 10^5
+
+# slow!
+def contains_nearby_duplicate(nums, k):
+    d = defaultdict(list)
+
+    for i in range(len(nums)):
+        d[nums[i]].append(i)
+
+    for key,val in d.items():
+        for i in range(1, len(val)):
+            if abs(val[i] - val[i-1]) <= k:
+                return True
+
+    return False
+
+# TODO: faster, less mem
+def contains_nearby_duplicate2(nums, k):
+    pass
+
+class TestContainsDupes(unittest.TestCase):
+    def test_empty(self):
+        nums = []
+        k = 0
+        self.assertFalse(contains_nearby_duplicate(nums, k))
+
+    def test_true(self):
+        nums = [1,2,3,1]
+        k = 3
+        self.assertTrue(contains_nearby_duplicate(nums, k))
+
+    def test_true2(self):
+        nums = [1,0,1,1]
+        k = 1
+        self.assertTrue(contains_nearby_duplicate(nums, k))
+
+    def test_false(self):
+        nums = [1,2,3,1,2,3]
+        k = 2
+        self.assertFalse(contains_nearby_duplicate(nums, k))
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
