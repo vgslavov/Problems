6179f4041176979731c1b1f1a96e4101056759a8
Vasil Slavov <vgslavov@umkc.edu>
2024-02-28 23:31:38 -0500

    Add valid parentheses

---
+++ leetcode/valid_parentheses.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# 1 <= s.length <= 10^4
+# s consists of parentheses only '()[]{}'.
+# TODO: refactor ugly code
+def valid_parentheses(s):
+    stack = []
+
+    for i in range(len(s)):
+        if s[i] == '(' or s[i] == '{' or s[i] == '[':
+            stack.append(s[i])
+        elif s[i] == ')':
+            if not stack or stack[-1] != '(':
+                return False
+            else:
+                stack.pop()
+        elif s[i] == '}':
+            if not stack or stack[-1] != '{':
+                return False
+            else:
+                stack.pop()
+        elif s[i] == ']':
+            if not stack or stack[-1] != '[':
+                return False
+            else:
+                stack.pop()
+        else:
+            print("invalid char")
+
+    return not len(stack)
+
+class TestValidParentheses(unittest.TestCase):
+    def test_empty(self):
+        s = ""
+        self.assertTrue(valid_parentheses(s))
+
+    def test_true1(self):
+        s = "()"
+        self.assertTrue(valid_parentheses(s))
+
+    def test_true2(self):
+        s = "()[]{}"
+        self.assertTrue(valid_parentheses(s))
+
+    def test_false1(self):
+        s = "(]"
+        self.assertFalse(valid_parentheses(s))
+
+    def test_false2(self):
+        s = "([)]"
+        self.assertFalse(valid_parentheses(s))
+
+    def test_false3(self):
+        s = "["
+        self.assertFalse(valid_parentheses(s))
+
+    def test_false4(self):
+        s = "]"
+        self.assertFalse(valid_parentheses(s))
+
+    def test_false5(self):
+        s = "){"
+        self.assertFalse(valid_parentheses(s))
+
+    def test_false6(self):
+        s = "(){}}{"
+        self.assertFalse(valid_parentheses(s))
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
