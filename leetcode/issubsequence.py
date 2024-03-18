e3706a21d1587a90a63f701f0989a0e443519905
Vasil Slavov <vgslavov@umkc.edu>
2024-03-17 22:26:45 -0400

    Refactor is subseq

--- leetcode/issubsequence.py
+++ leetcode/issubsequence.py
@@ -8,36 +8,17 @@ import unittest
 # 0 <= t.length <= 10^4
 # s and t consist only of lowercase English letters.
 
-# bad: matches out of order 
-def issubsequence1(s, t):
-    d = defaultdict(int)
-
-    for c in t:
-        d[c] += 1
-
-    for c in s:
-        if c not in d:
-            return False
-
-    return True
-
-# O(n): slow?
-def issubsequence2(s, t):
-    i = j = match = 0
+# O(n)
+def issubsequence(s, t):
+    i = j = 0
 
     while i < len(t) and j < len(s):
-        if s[j] != t[i]:
-            i += 1
-        elif s[j] == t[i]:
-            match += 1
-            i += 1
+        if s[j] == t[i]:
             j += 1
 
-    return match == len(s)
+        i += 1
 
-# TODO: O(n)
-def issubsequence3(s, t):
-    pass
+    return len(s) == j
 
 # TODO: extra
 # Suppose there are lots of incoming s, say s_1, s2_, ..., s_k where k >= 10^9,
@@ -48,22 +29,22 @@ class TestIssubsequence(unittest.TestCase):
     def test_empty(self):
         s = ''
         t = ''
-        self.assertTrue(issubsequence2(s, t))
+        self.assertTrue(issubsequence(s, t))
 
     def test_true(self):
         s = 'abc'
         t = 'ahbgdc'
-        self.assertTrue(issubsequence2(s, t))
+        self.assertTrue(issubsequence(s, t))
 
     def test_false1(self):
         s = 'axc'
         t = 'ahbgdc'
-        self.assertFalse(issubsequence2(s, t))
+        self.assertFalse(issubsequence(s, t))
 
     def test_false2(self):
         s = 'acb'
         t = 'ahbgdc'
-        self.assertFalse(issubsequence2(s, t))
+        self.assertFalse(issubsequence(s, t))
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
