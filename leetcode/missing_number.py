48d104100fc54ced2a9ee5a4d029effd9ab424d1
Vasil Slavov <vgslavov@umkc.edu>
2024-03-25 23:59:20 -0400

    Add missing number

---
+++ leetcode/missing_number.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# n == nums.length
+# 1 <= n <= 10^4
+# 0 <= nums[i] <= n
+# All the numbers of nums are unique.
+
+# O(n log n) runtime
+# O(1) space
+def missing_number1(nums):
+    nums.sort()
+
+    if nums and nums[0] != 0:
+        return 0
+
+    for i in range(len(nums)-1):
+        if nums[i] + 1 != nums[i+1]:
+            return nums[i] + 1
+
+    return nums[-1] + 1 if nums else False
+
+# O(n) runtime
+# O(n) space
+def missing_number2(nums):
+    if not nums:
+        return False
+
+    max_num = max(nums)
+    d = { n: 0 for n in range(max_num+1) }
+
+    for i in range(len(nums)):
+        d[nums[i]] += 1
+
+    for k,v in d.items():
+        if not v:
+            return k
+
+    return max_num + 1
+
+# O(n) runtime
+# O(1) space
+def missing_number3(nums):
+    if not nums:
+        return False
+
+    max_num = max(nums)
+    min_num = min(nums)
+
+    if min_num != 0:
+        return 0
+
+    real_sum = max_num * (max_num+1) // 2
+    actual_sum = sum(nums)
+
+    if real_sum == actual_sum:
+        return max_num + 1
+
+    return real_sum - actual_sum
+
+class TestMissingNumber(unittest.TestCase):
+    def test_empty(self):
+        nums = []
+        self.assertFalse(missing_number1(nums))
+        self.assertFalse(missing_number2(nums))
+        self.assertFalse(missing_number3(nums))
+
+    def test_0(self):
+        nums = [1]
+        expected = 0
+        self.assertEqual(missing_number1(nums), expected)
+        self.assertEqual(missing_number2(nums), expected)
+        self.assertEqual(missing_number3(nums), expected)
+
+    def test_1(self):
+        nums = [1,0]
+        expected = 2
+        self.assertEqual(missing_number1(nums), expected)
+        self.assertEqual(missing_number2(nums), expected)
+        self.assertEqual(missing_number3(nums), expected)
+
+    def test_2(self):
+        nums = [3,0,1]
+        expected = 2
+        self.assertEqual(missing_number1(nums), expected)
+        self.assertEqual(missing_number2(nums), expected)
+        self.assertEqual(missing_number3(nums), expected)
+
+    def test_3(self):
+        nums = [0,1]
+        expected = 2
+        self.assertEqual(missing_number1(nums), expected)
+        self.assertEqual(missing_number2(nums), expected)
+        self.assertEqual(missing_number3(nums), expected)
+
+    def test_4(self):
+        nums = [9,6,4,2,3,5,7,0,1]
+        expected = 8
+        self.assertEqual(missing_number1(nums), expected)
+        self.assertEqual(missing_number2(nums), expected)
+        self.assertEqual(missing_number3(nums), expected)
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
