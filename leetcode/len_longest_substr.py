03e87b1e8db9ac788006215a3cb297329a68bdbd
Vasil Slavov <vgslavov@umkc.edu>
2024-04-09 23:24:25 -0400

    Add WIP

---
+++ leetcode/len_longest_substr.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import sys
+import unittest
+
+# 0 <= s.length <= 5 * 10^4
+# s consists of English letters, digits, symbols and spaces.
+
+# TODO: WIP
+def len_longest_substr(s):
+    d = defaultdict(int)
+
+    left = ans = 0
+    ans_str = sub_str = ''
+
+    for right in range(len(s)):
+        if s[right] in d:
+            if len(sub_str) > len(ans_str):
+                ans_str = sub_str
+
+            left += 1
+
+        d[s[right]] += 1
+        sub_str += s[right]
+
+        ans = max(len(ans_str), len(sub_str))
+
+    return ans
+
+class TestLenLongestSubstr(unittest.TestCase):
+    def test_empty(self):
+        s = ''
+        expected = 0
+        self.assertEqual(len_longest_substr(s), expected)
+
+    def test_space(self):
+        s = ' '
+        expected = 1
+        self.assertEqual(len_longest_substr(s), expected)
+
+    def test_1(self):
+        s = "abcabcbb"
+        expected = 3
+        self.assertEqual(len_longest_substr(s), expected)
+
+    def test_2(self):
+        s = "bbbbb"
+        expected = 1
+        self.assertEqual(len_longest_substr(s), expected)
+
+    def test_3(self):
+        s = "pwwkew"
+        expected = 3
+        self.assertEqual(len_longest_substr(s), expected)
+
+    def test_4(self):
+        s = "dvdf"
+        expected = 3
+        self.assertEqual(len_longest_substr(s), expected)
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
---
+++ leetcode/max_depth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# The number of nodes in the tree is in the range [0, 10^4].
+# -100 <= Node.val <= 100
+# TODO: WIP
+def max_depth(root):
+    pass
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
