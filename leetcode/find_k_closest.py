51ff3be39af8a5d388c21e5681978631313c775a
Vasil Slavov <vgslavov@umkc.edu>
2024-06-12 00:37:23 -0400

    Add find_k_closest

---
+++ leetcode/find_k_closest.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import heapq
+import math
+import sys
+import unittest
+
+# 1 <= k <= points.length <= 10^4
+# -10^4 <= x_i, y_i <= 10^4
+
+# runtime O(n * log k): slow!
+# space O(n)
+def find_k_closest(points, k):
+    if not points:
+        return None
+
+    heap = []
+
+    for p in points:
+        dist = math.sqrt(pow(p[0] - 0, 2) + pow(p[1] - 0, 2))
+
+        heapq.heappush(heap, (dist, p))
+
+    ans = []
+    for i in range(k):
+        ans.append(heapq.heappop(heap)[1])
+
+    # TODO: avoid sort()
+    ans.sort()
+    return ans
+
+# runtime O(n * log k): faster!
+# space O(k)
+def find_k_closest2(points, k):
+    if not points:
+        return None
+
+    heap = []
+
+    for p in points:
+        dist = math.sqrt(pow(p[0] - 0, 2) + pow(p[1] - 0, 2))
+
+        # max heap!
+        heapq.heappush(heap, (-dist, p))
+
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    return [pair[1] for pair in heap]
+
+class TestFindKClosest(unittest.TestCase):
+
+    def test_empty(self):
+        points = []
+        k = 1
+        self.assertFalse(find_k_closest(points, k))
+        self.assertFalse(find_k_closest2(points, k))
+
+    def test_k1(self):
+        points = [[1,3],[-2,2]]
+        k = 1
+        expected = [[-2,2]]
+        self.assertEqual(expected, find_k_closest(points, k))
+        self.assertEqual(expected, find_k_closest2(points, k))
+
+    def test_k2(self):
+        points = [[3,3],[5,-1],[-2,4]]
+        k = 2
+        expected = [[-2,4],[3,3]]
+        self.assertEqual(expected, find_k_closest(points, k))
+        self.assertEqual(expected, find_k_closest2(points, k))
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
---
+++ leetcode/find_k_largest.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import heapq
+import sys
+import unittest
+
+# 1 <= k <= nums.length <= 10^5
+# -10^4 <= nums[i] <= 10^4
+
+# O(n * log k): slow?
+def find_k_largest1(nums, k):
+    if not nums:
+        return False
+    elif k == 1:
+        return max(nums)
+
+    return heapq.nlargest(k, nums)[-1]
+
+# O(n * log k): slow?
+def find_k_largest2(nums, k):
+    if k == 1:
+        return max(nums)
+
+    nums2 = [-n for n in nums]
+    heapq.heapify(nums2)
+
+    ans = None
+    for i in range(k):
+        ans = -heapq.heappop(nums2)
+
+    return ans
+
+# O(n * log k): slow?
+def find_k_largest3(nums, k):
+    if k == 1:
+        return max(nums)
+
+    # deep copy
+    h = list(nums)
+    heapq._heapify_max(h)
+
+    ans = None
+    for i in range(k):
+        ans = heapq._heappop_max(h)
+
+    return ans
+
+# O(n * log n): faster?
+def find_k_largest4(nums, k):
+    if not nums:
+        return False
+    elif k == 1:
+        return max(nums)
+
+    # or
+    #return sorted(nums)[-k:][0]
+    return sorted(nums, reverse=True)[:k][-1]
+
+# O(n * log n): fastest?
+def find_k_largest5(nums, k):
+    if not nums:
+        return False
+    elif k == 1:
+        return max(nums)
+
+    nums.sort()
+
+    return nums[-k:][0]
+
+class TestFindKthLargest(unittest.TestCase):
+    def test_empty(self):
+        nums = []
+        k = 0
+        self.assertFalse(find_k_largest1(nums, k))
+        self.assertFalse(find_k_largest2(nums, k))
+        self.assertFalse(find_k_largest3(nums, k))
+        self.assertFalse(find_k_largest4(nums, k))
+        self.assertFalse(find_k_largest5(nums, k))
+
+    def test_k1(self):
+        nums = [3,2,3,1,2,4,5,5,6]
+        k = 1
+        expected = 6
+        self.assertEqual(find_k_largest1(nums, k), expected)
+        self.assertEqual(find_k_largest2(nums, k), expected)
+        self.assertEqual(find_k_largest3(nums, k), expected)
+        self.assertEqual(find_k_largest4(nums, k), expected)
+        self.assertEqual(find_k_largest5(nums, k), expected)
+
+    def test_k2(self):
+        nums = [3,2,1,5,6,4]
+        k = 2
+        expected = 5
+        self.assertEqual(find_k_largest1(nums, k), expected)
+        self.assertEqual(find_k_largest2(nums, k), expected)
+        self.assertEqual(find_k_largest3(nums, k), expected)
+        self.assertEqual(find_k_largest4(nums, k), expected)
+        self.assertEqual(find_k_largest5(nums, k), expected)
+
+    def test_k4(self):
+        nums = [3,2,3,1,2,4,5,5,6]
+        k = 4
+        expected = 4
+        self.assertEqual(find_k_largest1(nums, k), expected)
+        self.assertEqual(find_k_largest2(nums, k), expected)
+        self.assertEqual(find_k_largest3(nums, k), expected)
+        self.assertEqual(find_k_largest4(nums, k), expected)
+        self.assertEqual(find_k_largest5(nums, k), expected)
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
