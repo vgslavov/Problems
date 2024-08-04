8c7d70456d652a4d806bbeb40d1e9e57fdca65aa
Vasil Slavov <vgslavov@umkc.edu>
2024-08-03 20:08:30 -0400

    WIP: longest seq

---
+++ leetcode/longest_seq.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import sys
+import unittest
+
+# 0 <= nums.length <= 10^5
+# -10^9 <= nums[i] <= 10^9
+# run-time: O(n)
+def longest_seq(nums):
+    if not nums:
+        return 0
+
+    counts = defaultdict(int)
+
+    for n in nums:
+        counts[n] += 1
+
+    for k,v in counts.items():
+        if not v:
+            continue
+        elif k-1 not in counts and k+1 not in counts:
+            counts[k] = 0 
+
+    print("counts:{}".format(counts))
+
+    nonzero = sum(1 for k in counts.keys() if counts[k])
+
+    return nonzero if nonzero else 1
+
+class TestLongestSeq(unittest.TestCase):
+
+    def test_empty(self):
+        nums = None
+        expected = 0
+        self.assertEqual(longest_seq(nums), expected)
+
+    def test_1(self):
+        nums = [100,4,200,1,3,2]
+        expected = 4
+        self.assertEqual(longest_seq(nums), expected)
+
+    def test_2(self):
+        nums = [0,3,7,2,5,8,4,6,0,1]
+        expected = 9
+        self.assertEqual(longest_seq(nums), expected)
+
+    def test_3(self):
+        nums = [0]
+        expected = 1
+        self.assertEqual(longest_seq(nums), expected)
+
+    def test_4(self):
+        nums = [0,0]
+        expected = 1
+        self.assertEqual(longest_seq(nums), expected)
+
+    def test_5(self):
+        nums = [0,0]
+        expected = 1
+        self.assertEqual(longest_seq(nums), expected)
+
+    # TODO: counting multiple sequences
+    def test_6(self):
+        nums = [9,1,4,7,3,-1,0,5,8,-1,6]
+        expected = 7
+        self.assertEqual(longest_seq(nums), expected)
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
