69f98badf0379faacc1961f1548717aaadc59e00
Vasil Slavov <vgslavov@umkc.edu>
2024-08-19 23:39:54 -0400

    Speed up hamming weight

--- leetcode/hamming_weight.py
+++ leetcode/hamming_weight.py
@@ -24,27 +24,44 @@ def hamming_weight(n):
 
     return count
 
+# solution: no division
+# complexity
+# run-time: O(n)?
+# space: O(1)
+def hamming_weight2(n):
+    count = 0
+
+    while n:
+        n &= n - 1
+        count += 1
+
+    return count
+
 class TestHammingWeight(unittest.TestCase):
 
     def test_0(self):
         n = 0
         expected = 0
         self.assertEqual(hamming_weight(n), expected)
+        self.assertEqual(hamming_weight2(n), expected)
 
     def test_11(self):
         n = 11
         expected = 3
         self.assertEqual(hamming_weight(n), expected)
+        self.assertEqual(hamming_weight2(n), expected)
 
     def test_128(self):
         n = 128
         expected = 1
         self.assertEqual(hamming_weight(n), expected)
+        self.assertEqual(hamming_weight2(n), expected)
 
     def test_2147483645(self):
         n = 2147483645
         expected = 30
         self.assertEqual(hamming_weight(n), expected)
+        self.assertEqual(hamming_weight2(n), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
