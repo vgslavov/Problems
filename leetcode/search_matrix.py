6f019cc1c831540cc1f8d159f71a9ab2416dc61a
Vasil Slavov <vgslavov@umkc.edu>
2024-09-08 15:56:24 -0400

    Use bisect

--- leetcode/search_matrix.py
+++ leetcode/search_matrix.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import bisect
 import sys
 import unittest
 
@@ -14,7 +15,6 @@ import unittest
 # 1 <= m, n <= 100
 # -10^4 <= matrix[i][j], target <= 10^4
 
-# solution: binary search x2
 # complexity
 # run-time: O(log(m) + log(n))
 # space: O(1)
@@ -34,10 +34,14 @@ def binary_search(nums, target):
 
     return left
 
+# complexity
+# run-time: O(log(m) + log(n))
+# space: O(1)
 def find_row(matrix, target):
     left = 0
     right = len(matrix)-1
 
+    # don't return None, that's False, same as 0th index
     if not matrix:
         return -1
     elif target < matrix[0][0] or target > matrix[-1][-1]:
@@ -46,7 +50,7 @@ def find_row(matrix, target):
     while left <= right:
         mid = left + (right-left) // 2
 
-        # equal to!
+        # equal to: need for 1-sized rows!
         if matrix[mid][0] <= target and target <= matrix[mid][-1]:
             return mid
         elif matrix[mid][0] > target:
@@ -56,6 +60,11 @@ def find_row(matrix, target):
 
     return -1
 
+# solution: manual binary search x2
+# complexity
+# run-time: O(log(m) + log(n))
+# space: O(1)
+# TODO: combine 2 steps?
 def search_matrix(matrix, target):
     row = find_row(matrix, target)
     #print('row:{}'.format(row))
@@ -68,26 +77,46 @@ def search_matrix(matrix, target):
 
     return False
 
+# solution: manual binary search + bisect
+# complexity
+# run-time: O(log(m) + log(n))
+# space: O(1)
+def search_matrix2(matrix, target):
+    row = find_row(matrix, target)
+    #print('row:{}'.format(row))
+    if row == -1:
+        return False
+
+    idx = bisect.bisect_left(matrix[row], target)
+    if idx < len(matrix[row]) and matrix[row][idx] == target:
+        return True
+
+    return False
+
 class TestSearchMatrix(unittest.TestCase):
     def test_empty(self):
         matrix = []
         target = None
         self.assertFalse(search_matrix(matrix, target))
+        self.assertFalse(search_matrix2(matrix, target))
 
     def test1(self):
         matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
         target = 3
         self.assertTrue(search_matrix(matrix, target))
+        self.assertTrue(search_matrix2(matrix, target))
 
     def test2(self):
         matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
         target = 13
         self.assertFalse(search_matrix(matrix, target))
+        self.assertFalse(search_matrix2(matrix, target))
 
     def test3(self):
         matrix = [[1]]
         target = 1
         self.assertTrue(search_matrix(matrix, target))
+        self.assertTrue(search_matrix2(matrix, target))
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
