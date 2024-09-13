cd4c82305f504cce0b8e0080509bf676eb4c5d80
Vasil Slavov <vgslavov@umkc.edu>
2024-09-12 23:52:09 -0400

    Fix search range

--- leetcode/search_range.py
+++ leetcode/search_range.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import bisect
 import sys
 import unittest
 
@@ -17,56 +18,93 @@ import unittest
 # complexity
 # run-time: O(log n)
 # space: O(1)
-def search_range(nums, target):
-    ans = [-1,-1]
-
-    # TODO: refactor
+def binary_search(nums, target):
     if not nums:
-        return ans
-    elif len(nums) == 1:
-        if nums[0] == target:
-            return [0,0]
-        else:
-            return ans
-    elif len(nums) == 2:
-        if nums[0] == target:
-            ans[0] = 0
-        elif nums[1] == target:
-            ans[0] = 1
-
-        if nums[1] == target:
-            ans[1] = 1
-        elif nums[0] == target:
-            ans[1] = 0
-
-        return ans
+        return -1
 
     left = 0
     right = len(nums)-1
 
     while left <= right:
         mid = left + (right-left) // 2
-        print(f"left:{left},mid:{mid},right:{right}")
+        #print(f"left:{left},mid:{mid},right:{right}")
 
         if nums[mid] == target:
-            if nums[mid-1] < target:
-                ans[0] = mid
-                left = mid+1
-            elif nums[mid+1] > target:
-                ans[1] = mid
-                right = mid-1
+            return mid
         elif nums[mid] < target:
             left = mid+1
         else:
             right = mid-1
 
-    # TODO: refactor
-    if ans[0] == -1 and nums[left] == target:
-        ans[0] = left
-    if ans[1] == -1 and nums[right] == target:
-        ans[1] = right
+    return left
 
-    return ans
+# solution: binary search + two pointers
+# complexity
+# run-time: O(log n)
+# space: O(1)
+def search_range(nums, target):
+    idx = binary_search(nums, target)
+    if idx < 0 or idx >= len(nums) or nums[idx] != target:
+        return [-1,-1]
+
+    #print(f"idx:{idx},val:{nums[idx]}")
+    left = right = idx
+    while 0 < left and right < len(nums)-1:
+        if nums[left-1] == target:
+            left -= 1
+
+        if nums[right+1] == target:
+            right += 1
+        else:
+            break
+
+    while 0 < left:
+        if nums[left-1] != target:
+            break
+
+        left -= 1
+
+    while right < len(nums)-1:
+        if nums[right+1] != target:
+            break
+
+        right += 1
+
+    return [left,right]
+
+# solution: Pythonic bisect + two pointers
+# complexity
+# run-time: O(log n)
+# space: O(1)
+def search_range2(nums, target):
+    idx = bisect.bisect_left(nums, target)
+    if idx < 0 or idx >= len(nums) or nums[idx] != target:
+        return [-1,-1]
+
+    #print(f"idx:{idx},val:{nums[idx]}")
+    left = right = idx
+    while 0 < left and right < len(nums)-1:
+        if nums[left-1] == target:
+            left -= 1
+
+        if nums[right+1] == target:
+            right += 1
+        else:
+            break
+
+    while 0 < left:
+        if nums[left-1] != target:
+            break
+
+        left -= 1
+
+    while right < len(nums)-1:
+        if nums[right+1] != target:
+            break
+
+        right += 1
+
+    return [left,right]
 
 class TestSearchRange(unittest.TestCase):
     def test_empty(self):
@@ -74,55 +112,77 @@ class TestSearchRange(unittest.TestCase):
         target = 0
         expected = [-1,-1]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test1(self):
         nums = [5,7,7,8,8,10]
         target = 8
         expected = [3,4]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test2(self):
         nums = [5,7,7,8,8,10]
         target = 6
         expected = [-1,-1]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test3(self):
         nums = [5,7,7,8,8,10]
         target = 6
         expected = [-1,-1]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test4(self):
         nums = [1]
         target = 1
         expected = [0,0]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test5(self):
         nums = [2,2]
         target = 2
         expected = [0,1]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test6(self):
         nums = [1,3]
         target = 1
         expected = [0,0]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
     def test7(self):
         nums = [1,2,3]
         target = 1
         expected = [0,0]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
-    # TODO: fix
     def test8(self):
         nums = [1,2,2]
         target = 2
-        expected = [0,1]
+        expected = [1,2]
         self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
+
+    def test9(self):
+        nums = [3,3,3]
+        target = 3
+        expected = [0,2]
+        self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
+
+    def test10(self):
+        nums = [2,2]
+        target = 3
+        expected = [-1,-1]
+        self.assertEqual(search_range(nums, target), expected)
+        self.assertEqual(search_range2(nums, target), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
