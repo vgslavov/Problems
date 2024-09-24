5998e3839e284400bb9b4ea4dda4dc5de731d808
Vasil Slavov <vgslavov@umkc.edu>
2024-09-24 15:50:39 -0400

    Solve pow

--- leetcode/pow.py
+++ leetcode/pow.py
@@ -20,14 +20,14 @@ import unittest
 # space: O(1)
 def pow(x, n):
     isneg = False
+
     if not n:
         return 1
     elif n < 0:
         n *= -1
         isneg = True
 
-    ans = x
-    n -= 1
+    ans = 1
 
     while n:
         ans *= x
@@ -38,14 +38,61 @@ def pow(x, n):
 # solution: brute-force recursive
 # complexity
 # run-time: O(n), slow!
-# space: O(1)
+# space: O(n)?
 def pow2(x, n):
+    # base case
+    if not n:
+        return 1
+    # negative: take reciprocal
+    elif n < 0:
+        return 1.0 / pow2(x, -n)
+
+    return x * pow2(x, n-1)
+
+# solution: recursive binary exponentiation
+# complexity
+# run-time: O(log n)
+# space: O(log n)
+def pow3(x, n):
+    # base case
+    if not n:
+        return 1
+    # negative: take reciprocal (but don't //)
+    elif n < 0:
+        return 1.0 / pow3(x, -n)
+
+    # n even: (x^2)^(n/2)
+    if n % 2 == 0:
+        return pow3(x*x, n//2)
+    # n odd: x*(x^2)^(n-1)/2
+    else:
+        return x * pow3(x*x, (n-1)//2)
+
+# solution: iterative binary exponentiation
+# complexity
+# run-time: O(log n)
+# space: O(log n)
+def pow4(x, n):
+    isneg = False
+
     if not n:
         return 1
     elif n < 0:
-        return 1 / pow(x, -n)
+        n *= -1
+        isneg = True
 
-    return x * pow2(x, n-1)
+    ans = 1
+
+    while n:
+        # odd
+        if n % 2 == 1:
+            ans *= x
+            n -= 1
+
+        x *= x
+        n //= 2
+
+    return 1/ans if isneg else ans
 
 class TestPow(unittest.TestCase):
     def test_0(self):
@@ -54,6 +101,8 @@ class TestPow(unittest.TestCase):
         expected = 1
         self.assertEqual(pow(x, n), expected)
         self.assertEqual(pow2(x, n), expected)
+        self.assertEqual(pow3(x, n), expected)
+        self.assertEqual(pow4(x, n), expected)
 
     def test1(self):
         x = 2.00000
@@ -61,6 +110,8 @@ class TestPow(unittest.TestCase):
         expected = 1024.0
         self.assertEqual(pow(x, n), expected)
         self.assertEqual(pow2(x, n), expected)
+        self.assertEqual(pow3(x, n), expected)
+        self.assertEqual(pow4(x, n), expected)
 
     def test2(self):
         x = 2.10000
@@ -68,14 +119,28 @@ class TestPow(unittest.TestCase):
         expected = 9.26100
         self.assertEqual(round(pow(x, n), 5), expected)
         self.assertEqual(round(pow2(x, n), 5), expected)
+        self.assertEqual(round(pow3(x, n), 5), expected)
+        self.assertEqual(round(pow4(x, n), 5), expected)
 
     def test_large(self):
         x = 0.00001
         n = 2147483647
         expected = 0
-        # TODO: too slow
+        # too slow
         #self.assertEqual(pow(x, n), expected)
         #self.assertEqual(pow2(x, n), expected)
+        self.assertEqual(pow3(x, n), expected)
+        self.assertEqual(pow4(x, n), expected)
+
+    def test_large2(self):
+        x = 2.00000
+        n = -2147483648
+        expected = 0
+        # too slow
+        #self.assertEqual(pow(x, n), expected)
+        #self.assertEqual(pow2(x, n), expected)
+        self.assertEqual(pow3(x, n), expected)
+        self.assertEqual(pow4(x, n), expected)
 
     def test_neg(self):
         x = 2.00000
@@ -83,6 +148,8 @@ class TestPow(unittest.TestCase):
         expected = 0.25000
         self.assertEqual(round(pow(x, n), 5), expected)
         self.assertEqual(round(pow2(x, n), 5), expected)
+        self.assertEqual(round(pow3(x, n), 5), expected)
+        self.assertEqual(round(pow4(x, n), 5), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
