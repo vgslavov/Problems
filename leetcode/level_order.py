15a9de5a16bac450db282f5b90a3204b2e983445
Vasil Slavov <vgslavov@umkc.edu>
2024-09-24 18:02:16 -0400

    Solve level order

---
+++ leetcode/level_order.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from collections import deque
+import sys
+import unittest
+
+# number: 102
+# section: binary tree bfs
+# difficulty: medium
+# tags: tree, bfs, binary tree, top 150
+
+# constraints
+# The number of nodes in the tree is in the range [0, 2000].
+# -1000 <= Node.val <= 1000
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# solution: iterative bfs
+# complexity
+# run-time: O(n)
+# space: O(n)
+def level_order(root):
+    if not root:
+        return []
+
+    queue = deque([root])
+
+    ans = []
+
+    while queue:
+        level_len = len(queue)
+
+        # or list comprehension
+        #ans.append([node.val for node in queue])
+        ans.append([])
+
+        for _ in range(level_len):
+            node = queue.popleft()
+
+            ans[-1].append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+    return ans
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
