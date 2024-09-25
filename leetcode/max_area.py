bfeee0fed171949ee900981c0865413bbab7bdb7
Vasil Slavov <vgslavov@umkc.edu>
2024-09-24 20:12:28 -0400

    Solve max area

---
+++ leetcode/max_area.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# number: 11
+# section: two pointers
+# difficulty: medium
+# tags: array, two pointers, greedy, top 150
+
+# constraints
+# n == height.length
+# 2 <= n <= 10^5
+# 0 <= height[i] <= 10^4
+
+# solution: two pointers
+# complexity
+# run-time: O(n)
+# space: O(1)
+def max_area(height):
+    i = 0
+    j = len(height)-1
+
+    ans = 0
+    while i < j:
+        l = j - i
+        h = min(height[i], height[j])
+        ans = max(ans, l*h)
+
+        if height[i] < height[j]:
+            i += 1
+        else:
+            j -= 1
+
+    return ans
+
+class TestMaxArea(unittest.TestCase):
+
+    def test_empty(self):
+        height = []
+        expected = 0
+        self.assertEqual(max_area(height), expected)
+
+    def test1(self):
+        height = [1,8,6,2,5,4,8,3,7]
+        expected = 49
+        self.assertEqual(max_area(height), expected)
+
+    def test2(self):
+        height = [1,1]
+        expected = 1
+        self.assertEqual(max_area(height), expected)
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
