97657518b94d68c5308638ce7394a18cbac63fd7
Vasil Slavov <vgslavov@umkc.edu>
2024-10-09 22:57:57 -0400

    Manual combinations calc

--- leetcode/domino_pairs.py
+++ leetcode/domino_pairs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
+from functools import cache
 import math
 import sys
 import unittest
@@ -8,14 +9,14 @@ import unittest
 # number: 1128
 # section: assessments
 # difficulty: easy
-# tags: amazon
+# tags: array, hash table, counting, amazon
 
 # constraints
 # 1 <= dominoes.length <= 4 * 104
 # dominoes[i].length == 2
 # 1 <= dominoes[i][j] <= 9
 
-# solution: dict + comb
+# solution: dict + math.comb
 # complexity
 # run-time: O(n)
 # space: O(n)
@@ -28,21 +29,91 @@ def domino_pairs(dominoes) -> int:
 
     return sum([math.comb(v,2) for v in counts.values()])
 
+# complexity
+# run-time:
+# space:
+def combinations(n, k):
+    #print(f"n:{n},k:{k}")
+
+    @cache
+    def dp(i):
+        # base case
+        if i <= 1:
+            return 1
+
+        # check cache
+        #if i in memo:
+        #    return memo[i]
+
+        # recurrence relation
+        #memo[i] = dp(i-1)*i
+        #return memo[i]
+
+        return dp(i-1)*i
+
+    #memo = {}
+    n_f = dp(n)
+    nk_f = dp(n-k)
+    k_f = dp(k)
+
+    # integer division: for n < k!
+    return n_f // (nk_f*k_f)
+
+# solution: dict + dp comb
+# complexity
+# run-time: O(n)
+# space: O(n)
+def domino_pairs2(dominoes) -> int:
+    ans = 0
+    counts = defaultdict(int)
+
+    for i in range(len(dominoes)):
+        counts[tuple(sorted(dominoes[i]))] += 1
+
+    return sum([combinations(v,2) for v in counts.values()])
+
 class TestDominoPairs(unittest.TestCase):
+    def test_combinations1(self):
+        n = 6
+        k = 2
+        expected = 15
+        self.assertEqual(combinations(n, k), expected)
+
+    def test_combinations2(self):
+        n = 2
+        k = 2
+        expected = 1
+        self.assertEqual(combinations(n, k), expected)
+
+    def test_combinations3(self):
+        n = 3
+        k = 2
+        expected = 3
+        self.assertEqual(combinations(n, k), expected)
+
+    def test_combinations4(self):
+        n = 1
+        k = 2
+        expected = 0
+        self.assertEqual(combinations(n, k), expected)
+
     def test_empty(self):
         dominoes = []
         expected = 0
         self.assertEqual(domino_pairs(dominoes), expected)
+        self.assertEqual(domino_pairs2(dominoes), expected)
 
     def test1(self):
         dominoes = [[1,2],[2,1],[3,4],[5,6]]
         expected = 1
         self.assertEqual(domino_pairs(dominoes), expected)
+        self.assertEqual(domino_pairs2(dominoes), expected)
 
     def test2(self):
         dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
         expected = 3
         self.assertEqual(domino_pairs(dominoes), expected)
+        self.assertEqual(domino_pairs2(dominoes), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
