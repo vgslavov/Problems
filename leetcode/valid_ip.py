4e5e28af5551488a8cf54a515a3094bd745ee26d
Vasil Slavov <vgslavov@umkc.edu>
2024-10-15 22:04:37 -0400

    Refactor

--- leetcode/valid_ip.py
+++ leetcode/valid_ip.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from ipaddress import ip_address, IPv4Address
 import re
 import sys
 import unittest
@@ -12,26 +13,40 @@ import unittest
 # constraints
 # queryIP consists only of English letters, digits and the characters '.' and ':'.
 
+# complexity
+# run-time: O(n)
+# space: O(1)
 def valid_ipv4(tokens):
+    # need exactly 4 tokens
     if not tokens or len(tokens) != 4:
         return "Neither"
 
     for i in range(len(tokens)):
         #print(f"tokens[{i}]:{tokens[i]}")
 
-        if not tokens[i].isdecimal():
+        # not necessary if try/except
+        #if not tokens[i].isdecimal():
+        #    return "Neither"
+
+        try:
+            num = int(tokens[i])
+        except (ValueError):
             return "Neither"
 
-        num = int(tokens[i])
         if num < 0 or num > 255:
             return "Neither"
 
+        # number can't start with 0
         if len(tokens[i]) > 1 and int(tokens[i][0]) == 0:
             return "Neither"
 
     return "IPv4"
 
+# complexity
+# run-time: O(n)
+# space: O(1)
 def valid_ipv6(tokens):
+    # need exactly 8 tokens
     if not tokens or len(tokens) != 8:
         return "Neither"
 
@@ -46,7 +61,7 @@ def valid_ipv6(tokens):
 
     return "IPv6"
 
-# solution: manual
+# solution: Pythonic split + divide & conquer + try/except
 # complexity
 # run-time: O(n)
 # space: O(1)
@@ -54,12 +69,14 @@ def valid_ip(query: str) -> str:
     if not query:
         return "Neither"
 
+    # check if IPv4
     tokens = query.split(".")
 
     if len(tokens) > 1:
         return valid_ipv4(tokens)
-    else:
-        tokens = query.split(":")
+
+    # check if IPv6
+    tokens = query.split(":")
 
     if len(tokens) > 1:
         return valid_ipv6(tokens)
@@ -70,7 +87,7 @@ def valid_ip(query: str) -> str:
 # complexity
 # run-time: O(n)
 # space: O(1)
-# TODO: finish
+# TODO: finish solving using re
 def valid_ip2(query: str) -> str:
     r = "( 25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]? )"
     r2 = [r]*4
@@ -82,32 +99,50 @@ def valid_ip2(query: str) -> str:
 
     return "Neither"
 
+# solution: Pythonic, non-interview
+def valid_ip3(query):
+    try:
+        return "IPv4" if type(ip_address(query)) is IPv4Address else "IPv6"
+    except ValueError:
+        return "Neither"
+
 class TestValidIP(unittest.TestCase):
     def test_empty(self):
         query = ""
         expected = "Neither"
         self.assertEqual(valid_ip(query), expected)
+        self.assertEqual(valid_ip3(query), expected)
 
     def test_ipv4_valid(self):
         query = "172.16.254.1"
         expected = "IPv4"
         self.assertEqual(valid_ip(query), expected)
         #self.assertEqual(valid_ip2(query), expected)
+        self.assertEqual(valid_ip3(query), expected)
 
     def test_ipv4_invalid(self):
         query = "256.256.256.256"
         expected = "Neither"
         self.assertEqual(valid_ip(query), expected)
+        self.assertEqual(valid_ip3(query), expected)
 
     def test_ipv4_invalid2(self):
         query = "01.01.01.01"
         expected = "Neither"
         self.assertEqual(valid_ip(query), expected)
+        self.assertEqual(valid_ip3(query), expected)
+
+    def test_ipv4_invalid3(self):
+        query = "0db8.01.01.01"
+        expected = "Neither"
+        self.assertEqual(valid_ip(query), expected)
+        self.assertEqual(valid_ip3(query), expected)
 
     def test_ipv6_valid(self):
         query = "2001:0db8:85a3:0:0:8A2E:0370:7334"
         expected = "IPv6"
         self.assertEqual(valid_ip(query), expected)
+        self.assertEqual(valid_ip3(query), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
