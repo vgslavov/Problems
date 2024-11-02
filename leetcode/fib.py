2cbb11a74f23823ec8e54c69536c84b2757c3429
Vasil Slavov <vgslavov@umkc.edu>
2024-11-01 22:23:53 -0400

    Iterative fib

--- leetcode/fib.py
+++ leetcode/fib.py
@@ -12,7 +12,7 @@ import unittest
 # constraints
 # 0 <= n <= 30
 
-# solution: top-down recursive DP using functools.cache for memoization
+# solution: top-down recursive DP using functools for memoization
 # complexity
 # run-time: O(n)
 # space: O(n)
@@ -48,22 +48,46 @@ def fib2(n: int) -> int:
     memo = {}
     return dp(n)
 
+# solution: bottom up iterative DP
+# complexity
+# run-time: O(n)
+# space: O(n)
+def fib3(n: int) -> int:
+    # init
+    d = [0] * (n+1)
+
+    # base cases
+    if not n:
+        return 0
+
+    d[1] = 1
+
+    # recurrence relation
+    for i in range(2, n+1):
+        d[i] = d[i-1] + d[i-2]
+
+    return d[n]
+
 class TestFib(unittest.TestCase):
     def test0(self):
         self.assertEqual(fib(0), 0)
         self.assertEqual(fib2(0), 0)
+        self.assertEqual(fib3(0), 0)
 
     def test2(self):
         self.assertEqual(fib(2), 1)
         self.assertEqual(fib2(2), 1)
+        self.assertEqual(fib3(2), 1)
 
     def test3(self):
         self.assertEqual(fib(3), 2)
         self.assertEqual(fib2(3), 2)
+        self.assertEqual(fib3(3), 2)
 
     def test4(self):
         self.assertEqual(fib(4), 3)
         self.assertEqual(fib2(4), 3)
+        self.assertEqual(fib3(4), 3)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
