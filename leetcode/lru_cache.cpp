86d943c355f3a02ef55ef32e85c1056fcb9b84ea
Vasil Slavov <vgslavov@outlook.com>
2024-11-10 09:41:11 -0500

    Refactor to rm code duplication

--- leetcode/lru_cache.cpp
+++ leetcode/lru_cache.cpp
@@ -19,62 +19,35 @@
 // complexity
 // run-time: O(1) for both put/get
 // space: O(capacity)
+#include <list>
+#include <stdexcept>
+#include <tuple>
+#include <unordered_map>
+
 class LRUCache {
 public:
     LRUCache(int capacity)
-    : d_capacity(capacity)
-    {
+    : d_capacity(capacity) {
         if (capacity <= 0) {
             throw std::invalid_argument("invalid capacity"); 
         }
     }
 
     int get(int key) {
-        KeyListMap::iterator cacheIter = d_cache.find(key);
-
-        // not found
-        if (cacheIter == d_cache.end()) {
-            return -1;
-        }
-
-        // remove from list
-        ListTuples::iterator listIter = cacheIter->second;
-        int value = std::get<1>(*listIter);
-        d_list.erase(listIter);
-
-        // add back to List at end
-        d_list.push_back(std::tuple(key, value));
-
-        // get iter to last element, not end placeholder
-        d_cache[key] = std::prev(d_list.end());
-
-        return value;
+        // refresh: make recently used if found
+        return update(key, -1, false);
     }
 
     void put(int key, int value) {
-        KeyListMap::iterator cacheIter = d_cache.find(key);
-
-        // update existing key
-        if (cacheIter != d_cache.end()) {
-            ListTuples::iterator listIter = cacheIter->second;
-            d_list.erase(listIter);
-
-            // move to end
-            d_list.push_back(std::make_tuple(key, value));
-
-            // save iterator
-            d_cache[key] = std::prev(d_list.end());
-
+        // update existing key if found
+        if (update(key, value) != -1) {
             return;
         }
 
         // at capacity
         if (d_cache.size() == d_capacity) {
-            // remove lru key from cache
             int lruKey = std::get<0>(d_list.front());
             d_cache.erase(lruKey);
-
-            // remove lru key from list
             d_list.pop_front();
         }
 
@@ -86,12 +59,31 @@ public:
     }
 
 private:
-    // (key, value) tuples
+    // refersh: make recently used if found & update
+    int update(int key, int value, bool overwrite=true) {
+        KeyListMap::iterator cacheIter = d_cache.find(key);
+        if (cacheIter == d_cache.end()) {
+            return -1;
+        }
+
+        ListTuples::iterator listIter = cacheIter->second;
+        if (!overwrite) {
+            value = std::get<1>(*listIter);
+        }
+
+        d_list.erase(listIter);
+        d_list.push_back(std::tuple(key, value));
+
+        // get iter to last element, not end placeholder
+        d_cache[key] = std::prev(d_list.end());
+
+        return value;
+    }
+
+    // key, value tuples
     using ListTuples = std::list<std::tuple<int, int>>;
-
-    // key to List iterator
+    // key: List iterator
     using KeyListMap = std::unordered_map<int, ListTuples::iterator>;
-
     KeyListMap d_cache;
     ListTuples d_list;
     int d_capacity;
