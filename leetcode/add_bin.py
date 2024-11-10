b1dc2943818eb64b6f7dfd12aedfe59b6867c9c2
Vasil Slavov <vgslavov@outlook.com>
2024-11-10 16:02:18 -0500

    Adding binaries using bitwise ops

--- leetcode/add_bin.py
+++ leetcode/add_bin.py
@@ -13,9 +13,9 @@ import unittest
 # a and b consist only of '0' or '1' characters.
 # Each string does not contain leading zeros except for the zero itself.
 
-# solution: Pythonic int/bin
-# run-time: O(1)
-# space: O(1)
+# solution: Pythonic int/bin & addition
+# run-time: O(n+m)
+# space: O(max(n,m))
 def add_bin(a, b):
     # convert binary string to int
     # add
@@ -23,7 +23,41 @@ def add_bin(a, b):
     # slice
     return bin(int(a, 2) + int(b, 2))[2:]
 
-# TODO: add unit tests & solve using bin arithmetic
+# solution: Leetcode Pythonic int/bin & bitwise operators
+# run-time: O(n+m)
+# space: O(max(n,m))
+def add_bin2(a, b):
+    # convert binary to int
+    x, y = int(a, 2), int(b, 2)
+
+    # while carry is non-0
+    while y:
+        # w/o carry
+        answer = x ^ y
+        # add carry
+        carry = (x & y) << 1
+
+        # store answer in x, carry in y
+        x, y = answer, carry
+
+        # one-liner
+        #x, y = x ^ y, (x & y) << 1
+
+    # drop "0b" prefix
+    return bin(x)[2:]
+
+class TestAddBin(unittest.TestCase):
+    def test1(self):
+        a = "11"
+        b = "1"
+        expected = "100"
+        self.assertEqual(add_bin(a, b), expected)
+
+    def test2(self):
+        a = "1010"
+        b = "1011"
+        expected = "10101"
+        self.assertEqual(add_bin(a, b), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
