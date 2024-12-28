8d49e82612e2905db7c1d7a7c053294a5f862235
Vasil Slavov <vgslavov@outlook.com>
2024-12-27 22:22:45 -0500

    Solve swap pairs in C++

---
+++ leetcode/swap_pairs.cpp
@@ -0,0 +1,48 @@
+#include <utility>
+
+// number: 24
+// section: recursion
+// difficulty: medium
+// tags: linked list, recursion
+
+// constraints
+// The number of nodes in the list is in the range [0, 100].
+// 0 <= Node.val <= 100
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void swapPairsHelper(ListNode* head)
+{
+    // base case
+    if (!head || !head->next) {
+        return;
+    }
+
+    std::swap(head->val, head->next->val);
+
+    return swapPairsHelper(head->next->next);
+}
+
+// solution: swap values
+// complexity
+// run-time: O(n)
+// space: O(n)
+ListNode* swapPairs(ListNode* head)
+{
+    if (!head || !head->next) {
+        return head;
+    }
+
+    ListNode* orgHead = head;
+
+    swapPairsHelper(head);
+
+    return orgHead;
+}
