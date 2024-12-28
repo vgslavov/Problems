a5c098482bc12140ee58db666516ec814b4a0ff1
Vasil Slavov <vgslavov@outlook.com>
2024-12-27 22:14:11 -0500

    Solve max profit 2 in C++

---
+++ leetcode/max_profit2.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+// number: 122
+// section: array/string
+// difficulty: medium
+// tags: array, dp, greedy, top 150
+
+// constraints
+// 1 <= prices.length <= 3 * 10^4
+// 0 <= prices[i] <= 10^4
+// multiple buys & sells
+// but hold at most one share of stock at a time
+
+// solution: Leetcode greedy
+// complexity
+// run-time: O(n)
+// space: O(1)
+int maxProfit(const std::vector<int>& prices) {
+    int profit = 0;
+    for (size_t i = 1; i != prices.size(); ++i) {
+        if (prices[i] > prices[i-1]) {
+            profit += prices[i]-prices[i-1];
+        }
+    }
+
+    return profit;
+}
+
+// TODO: solve using DP & add unit tests
