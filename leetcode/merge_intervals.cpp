93bbf687dfa3e00f03c353a58c9f04d2362a3cc6
Vasil Slavov <vgslavov@outlook.com>
2025-01-03 15:48:22 -0500

    Solve merge intervals in C++

---
+++ leetcode/merge_intervals.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <vector>
+
+// number: 56
+// section: intervals
+// difficulty: medium
+// tags: array, sorting, top 150, meta
+
+// constraints
+// 1 <= intervals.length <= 10^4
+// intervals[i].length == 2
+// 0 <= start_i <= end_i <= 10^4
+
+// solution: sort + min/max
+// complexity
+// run-time: O(n*log n)
+// space: O(1)!
+std::vector<std::vector<int>> mergeIntervals(
+        std::vector<std::vector<int>>& intervals)
+{
+    std::sort(intervals.begin(), intervals.end());
+
+    int i = 1;
+    while (i < intervals.size()) {
+        if (intervals[i][0] <= intervals[i-1][1]) {
+            intervals[i][0] = std::min(intervals[i][0], intervals[i-1][0]);
+            intervals[i][1] = std::max(intervals[i][1], intervals[i-1][1]);
+            // TODO: more efficient?
+            intervals.erase(intervals.begin() + i-1);
+        } else {
+            ++i;
+        }
+    }
+
+    return intervals;
+}
+
+// TODO: add unit tests
