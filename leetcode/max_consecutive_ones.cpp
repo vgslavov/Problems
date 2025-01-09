e267241cd2d1c53461c2f50f0eea4fd35550f436
Vasil Slavov <vgslavov@outlook.com>
2025-01-08 23:06:04 -0500

    Solve max consecutive ones in C++

---
+++ leetcode/max_consecutive_ones.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <vector>
+
+// number: 1004
+// section: sliding window
+// difficulty: medium
+// tags: array, binary search, sliding window, prefix sum, leetcode 75
+
+// constraints
+// 1 <= nums.length <= 10^5
+// nums[i] is either 0 or 1.
+// 0 <= k <= nums.length
+// k: how many 0s to flip
+
+// complexity
+// run-time: O(n)
+// space: O(1)
+int maxConsecutiveOnes(std::vector<int>& nums, int k)
+{
+    int left = 0;
+    int ans = 0;
+    int curr = 0;
+
+    for (int right = 0; right != nums.size(); ++right) {
+        // count 0s
+        if (nums[right] == 0) {
+            ++curr;
+        }
+
+        while (curr > k) {
+            // remove 0s
+            if (nums[left] == 0) {
+                --curr;
+            }
+
+            // shrink window
+            ++left;
+        }
+
+        ans = std::max(ans, right-left+1);
+    }
+
+    return ans;
+}
