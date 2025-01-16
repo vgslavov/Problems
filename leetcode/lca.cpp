4f87641638eba0e771e2f10389f1a172720042a3
Vasil Slavov <vgslavov@outlook.com>
2025-01-15 22:31:13 -0500

    Solve LCA

---
+++ leetcode/lca.cpp
@@ -0,0 +1,55 @@
+#include <cstddef>
+
+// number: 236
+// section: binary tree
+// difficulty: medium
+// tags: tree, dfs, binary tree, top 150
+
+// constraints
+// The number of nodes in the tree is in the range [2, 105].
+// -109 <= Node.val <= 109
+// All Node.val are unique.
+// p != q
+// p and q will exist in the tree.
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// solution: recursive dfs
+// complexity
+// run-time: O(n)
+// space: O(n)
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+{
+    // empty tree
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    // 1) p or q is root, root is LCA
+    if (root == p || root == q) {
+        return root;
+    }
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    // 2) p and q in different subtrees, root is LCA
+    if (left != nullptr && right != nullptr) {
+        return root;
+    }
+
+    // 3) p and q in same subtree
+    if (left != nullptr) {
+        return left;
+    }
+
+    return right;
+}
+
+// TODO: add unit tests
---
+++ leetcode/lca.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+# number: 236
+# section: binary tree
+# difficulty: medium
+# tags: tree, dfs, binary tree, top 150
+
+# constraints
+# The number of nodes in the tree is in the range [2, 105].
+# -109 <= Node.val <= 109
+# All Node.val are unique.
+# p != q
+# p and q will exist in the tree.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# solution: recursive dfs
+# complexity
+# run-time: O(n)
+# space: O(n)
+def lca(root, p, q):
+    # empty tree
+    if not root:
+        return None
+
+    # 1) p or q is root, root is LCA
+    if root == p or root == q:
+        return root
+
+    left = lca(root.left, p, q)
+    right = lca(root.right, p, q)
+
+    # 2) p and q in different subtrees, root is LCA
+    if left and right:
+        return root
+
+    # 3) p and q in same subtree
+    if left:
+        return left
+
+    return right
+
+# TODO: add unit tests
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
