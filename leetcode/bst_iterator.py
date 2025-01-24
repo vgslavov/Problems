e51cd9ef907cbd294de7c7ecc4b08860a017e5cc
Vasil Slavov <vgslavov@outlook.com>
2025-01-24 00:11:46 -0500

    Solve BST iter in py

---
+++ leetcode/bst_iterator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# number: 173
+# section: binary tree general
+# difficulty: medium
+# tags: stack, tree, design, bst, binary tree, iterator
+
+# constraints
+# The number of nodes in the tree is in the range [1, 10^5].
+# 0 <= Node.val <= 10^6
+# At most 10^5 calls will be made to hasNext, and next.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BSTIterator:
+
+    # complexity
+    # run-time: O(n)
+    # space: O(n)
+    def __init__(self, root: Optional[TreeNode]):
+        self.nodes = []
+        self.index = -1
+        self.dfs(root)
+
+    # complexity
+    # run-time: O(1)
+    # space: O(n)
+    def next(self) -> int:
+        self.index += 1
+        return self.nodes[self.index] if self.index < len(self.nodes) else -1
+
+    # complexity
+    # run-time: O(1)
+    # space: O(n)
+    def hasNext(self) -> bool:
+        return self.index < len(self.nodes)-1
+
+    def dfs(self, root):
+        if not root:
+            return
+
+        self.dfs(root.left)
+        self.nodes.append(root.val)
+        self.dfs(root.right)
+
+# TODO: add unit tests & solve w/ O(log n) space
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
