b09c26682cbdbd4cfd0626e7ca5fb709b1b85120
Vasil Slavov <vgslavov@outlook.com>
2026-02-07 22:50:46 -0500

    Add 2nd group anagrams solution

--- leetcode/group_anagrams.py
+++ leetcode/group_anagrams.py
@@ -25,36 +25,55 @@ import unittest
 def group_anagrams(strs):
     d = defaultdict(list)
 
-    for i in range(len(strs)):
-        # sort each string
-        ss = ''.join(sorted(strs[i]))
-        # key: sorted str, value: list of original str
-        d[ss].append(i)
+    for s in strs:
+        # normalize to lowercase for key; keep original in value
+        ss = ''.join(sorted(s.lower()))
+        d[ss].append(s)
 
-    ans = []
-    for v in d.values():
-        ans.append([strs[i] for i in v])
+    return list(d.values())
 
-    return ans
+# solution: dict
+# complexity
+# run-time: O(n * k)
+# space: O(k)
+def group_anagrams2(strs):
+    d = defaultdict(list)
 
-# TODO: faster?
+    for s in strs:
+        # const space
+        counts = [0] * 26
+        for c in s.lower():
+            counts[ord(c) - ord('a')] += 1
+        d[tuple(counts)].append(s)
+
+    return list(d.values())
 
 class TestGroupAnagrams(unittest.TestCase):
     def test_empty(self):
         strs = [""]
         expected = [[""]]
         self.assertEqual(group_anagrams(strs), expected)
+        self.assertEqual(group_anagrams2(strs), expected)
 
     def test_1(self):
         strs = ["a"]
         expected = [["a"]]
         self.assertEqual(group_anagrams(strs), expected)
+        self.assertEqual(group_anagrams2(strs), expected)
 
     # order of expected doesn't matter
     def test_2(self):
         strs = ["eat","tea","tan","ate","nat","bat"]
         expected = [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
         self.assertEqual(group_anagrams(strs), expected)
+        self.assertEqual(group_anagrams2(strs), expected)
+
+    def test_uppercase(self):
+        # case-insensitive: normalize to lowercase for key, keep originals in groups
+        strs = ["Eat", "eat", "tea"]
+        expected = [["Eat", "eat", "tea"]]
+        self.assertEqual(group_anagrams(strs), expected)
+        self.assertEqual(group_anagrams2(strs), expected)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
